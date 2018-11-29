a125acbf315784d6a501d85eb7b416f7c898ca7b
Skratzy <ettafredrik@gmail.com>
2018-11-29 08:16:10 +0100

    Initial commit for PackageBuilder

--- ResourceManager/Main.cpp
+++ ResourceManager/Main.cpp
@@ -1,5 +1,16 @@
+#include <filesystem>
+#include <iostream>
+
 int main() {
+	std::hash<std::string> test;
+	std::string apa("rootfolder/tex/monkeh.apa");
+	std::string apa2("rootfolder/tex/monkey.apa");
 
+	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
+	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
+	std::cout << "Path [" << apa2 << "]  ID [" << test(apa2) << "]." << std::endl;
+	std::cout << "Path ["<< apa2 << "]  ID ["<< test(apa2) << "]." << std::endl;
 
+	getchar();
 	return 0;
 }
