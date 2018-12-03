9ba483312cf366448e66e70c425825f345c905ed
Skratzy <ettafredrik@gmail.com>
2018-12-03 14:03:59 +0100

    Removed code in main() to seperate functions

--- ResourceManager/Main.cpp
+++ ResourceManager/Main.cpp
@@ -2,21 +2,11 @@
 #include <iostream>
 #include <fstream>
 
-
-int main() {
-	std::hash<std::string> test;
-	std::string apa("rootfolder/tex/monkeh.apa");
-	std::string apa2("rootfolder/tex/monkey.apa");
-
-	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
-	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
-	std::cout << "Path [" << apa2 << "]  ID [" << test(apa2) << "]." << std::endl;
-	std::cout << "Path ["<< apa2 << "]  ID ["<< test(apa2) << "]." << std::endl;
-
+void unbufferedRead() {
 	std::ifstream fileStream;
 	fileStream.rdbuf()->pubsetbuf(0, 0);
 	fileStream.open("test.txt");
-	
+
 	// get length of file:
 	fileStream.seekg(0, fileStream.end);
 	int length = fileStream.tellg();
@@ -28,9 +18,20 @@ int main() {
 	std::string data(buffer, length);
 
 	std::cout << data << std::endl;
+}
 
-	//std::cout << doStuff<int>("stuff") << std::endl;
-	//Mesh* data = ResourceManager::load<Mesh>();
+void testHash() {
+	std::hash<std::string> test;
+	std::string apa("rootfolder/tex/monkeh.apa");
+	std::string apa2("rootfolder/tex/monkey.apa");
+
+	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
+	std::cout << "Path [" << apa << "]  ID [" << test(apa) << "]." << std::endl;
+	std::cout << "Path [" << apa2 << "]  ID [" << test(apa2) << "]." << std::endl;
+	std::cout << "Path [" << apa2 << "]  ID [" << test(apa2) << "]." << std::endl;
+}
+
+int main() {
 
 
 	getchar();
