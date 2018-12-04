9cad737cc70f016416c27b494a05e019418b4bc2
Emil Wahl <emilwahl@gmail.com>
2018-12-04 13:41:14 +0100

    Added debug messages to ResourceManager::load()

--- ResourceManager/ResourceManager.cpp
+++ ResourceManager/ResourceManager.cpp
@@ -1,4 +1,5 @@
 #include "ResourceManager.h"
+#include "Defines.h"
 
 #include <experimental/filesystem>
 
@@ -40,6 +41,9 @@ Resource * ResourceManager::load(const std::string & path)
 				res = FL->load(path, hashedPath);
 				// Update memory usage
 				m_memUsage += res->getSize();
+				if (m_memUsage > m_capacity) {
+					RM_DEBUG_MESSAGE("ResourceManager::load() - Memory usage exceeds the memory limit.", 0);
+				}
 				// Increase the reference count of the resource
 				res->refer();
 				// Add the loaded resource to the map
@@ -48,7 +52,7 @@ Resource * ResourceManager::load(const std::string & path)
 			}
 		}
 
-		throw std::exception(("Did not find a format loader that supported the extension '" + ext + "'").c_str());
+		RM_DEBUG_MESSAGE("ResourceManager::load() - Did not find a format loader that supported the extension '" + ext + "'", 0);
 	}
 
 	return res;
