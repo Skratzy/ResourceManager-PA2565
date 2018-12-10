6e32285828feb1edf1c9f455b35bc922079f883e
Emil Wahl <emilwahl@gmail.com>
2018-12-10 13:16:59 +0100

    Merged with Daniel

---
+++ PackageBuilder/JPGLoader.h
@@ -0,0 +1,30 @@
+#ifndef _PB_JPG_LOADER_H
+#define _PB_JPG_LOADER_H
+
+#include "FormatLoader.h"
+
+class Resource;
+
+class JPGLoader : public FormatLoader {
+public:
+	JPGLoader() {
+		this->m_supportedExtensions.push_back(".jpg");
+	}
+	~JPGLoader() {}
+	/*
+	Returns a resource with the following data:
+	- int height;
+	- int width;
+	- vector<unsigned char> image;
+	'image' is structured:
+	Pixel_0_R, 	Pixel_0_B,	Pixel_0_G,  Pixel_0_A, Pixel_1_R, Pixel_1_G etc.
+	'image' is read row-wise:
+	1------->
+	2------->
+	3------->
+	*/
+	our::string load(const std::string& path);
+	void write(our::string &data);
+};
+
+#endif
---
+++ PackageBuilder/PNGLoader.h
@@ -0,0 +1,31 @@
+#ifndef _PB_PNG_LOADER_
+#define _PB_PNG_LOADER_
+
+#include <string>
+
+#include "FormatLoader.h"
+#include "Resource.h"
+
+class Resource;
+
+class PNGLoader : public FormatLoader {
+public:
+	PNGLoader() { m_supportedExtensions.push_back(".png"); };
+	~PNGLoader() {};
+	/*
+	Returns a resource with the following data:
+	- int height;
+	- int width;
+	- vector<unsigned char> image;
+	'image' is structured:
+	Pixel_0_R, 	Pixel_0_B,	Pixel_0_G,  Pixel_0_A, Pixel_1_R, Pixel_1_G etc.
+	'image' is read row-wise:
+	1------->
+	2------->
+	3------->
+	*/
+	our::string load(const std::string& path);
+	void write(our::string &data);
+};
+
+#endif //_RM_PNG_LOADER_
